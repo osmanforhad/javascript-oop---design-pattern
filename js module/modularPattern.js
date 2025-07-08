6c48893cd37039602746ec6a99cc4e576fcefe86
osmanforhad <osmanforhad93@gmail.com>
2025-07-08 15:32:20 +0600

    working with js modualr pattern

---
+++ js module/modularPattern.js
@@ -0,0 +1,34 @@
+function createCounter() {
+
+    let count = 0;
+
+    //create a  clousers this is also called modular pattern
+    return {
+
+        increment: function () {
+            count++;
+            console.log(count);
+        },
+        decrement: function () {
+            count--;
+            console.log(count);
+        },
+
+        getCount: function () {
+            return count;
+        }
+
+    }
+
+}
+//create object instance for createCounter method
+const counter = new createCounter();
+
+counter.increment();//1
+counter.increment();//2
+counter.increment();//3
+
+counter.decrement();//3-1 = 2
+
+const result = counter.getCount();//2
+console.log(result);//2
