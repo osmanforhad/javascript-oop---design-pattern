c70e81becc082b515cde3c6f53e53a7385df8ab8
osmanforhad <osmanforhad93@gmail.com>
2025-07-08 13:34:46 +0600

    working with method overloading

---
+++ methodOverloading.js
@@ -0,0 +1,14 @@
+//Method OverLoading, same method name but paramiter number are different
+class MyClass {
+
+
+    myMethod(p1, p2) {
+
+    }
+
+    //OverLoading Method
+    myMethod(p1, p2, p3) {
+
+    }
+
+}
