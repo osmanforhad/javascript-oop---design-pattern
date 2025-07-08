447b2a5df241399830c8f70955b56842056d4709
osmanforhad <osmanforhad93@gmail.com>
2025-07-08 17:01:41 +0600

    working with single tone design pattern

---
+++ js singletone pattern/singleTone.js
@@ -0,0 +1,20 @@
+//Single Tone Design Pattern
+
+//create db connect object instance
+const CreateDatabaseConnection = {
+    connect: null,
+    //annonymous fucntion
+    createConnect: function () {
+        if (!this.connect) {
+            //Database Connection Code
+            this.connect = { message: "I am only on connection" }
+        }
+        return this.connect;
+    }
+}
+
+//create object instance and calling objets properties
+const obj1 = CreateDatabaseConnection.connect();
+const obj2 = CreateDatabaseConnection.connect();
+
+console.log(obj1 == obj2);
